ea226233f2450883d9219043eba190a14e46cb2b
ppaska <ppaska@falconsoft-ltd.com>
2020-04-30 19:51:42 +0100

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,9 +2,10 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
+  template: `<h2>Hello {{name}}!</h2>`,
   styles: [`h1 { font-family: Lato; }`]
 })
 export class HelloComponent  {
+
   @Input() name: string;
 }
